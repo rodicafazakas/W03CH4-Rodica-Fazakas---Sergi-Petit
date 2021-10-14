a971e77bb20ce3661976e03d3db6159b1bad915b
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-15 00:23:34 +0200

    Connect API to file structure

--- challenge4/public/Serie.js
+++ challenge4/public/Serie.js
@@ -2,12 +2,16 @@ class Serie {
     name;
     creator;
     year;
+    poster;
+    watched;
     score;
 
-    constructor(name, creator, year, score){
+    constructor(name, creator, year, poster, watched, score){
         this.name = name;
         this.creator = creator;
         this.year = year;
+        this.poster = poster; 
+        this.watched = watched;
         this.score = score;
     }
 };
--- challenge4/public/index.js
+++ challenge4/public/index.js
@@ -2,16 +2,18 @@ import Card from "./Card.js";
 import Componente from "./Componente.js";
 import SeriesService from "./SeriesService.js";
 
- const urlApi = "http://localhost:3001/series";
+const urlApi = "http://localhost:3001/series";
 
 import Serie from "./Serie.js"; 
 import Page from "./Page.js"
 
-const services = new SeriesService
-console.log(services.getSerie())
-
-const theSopranos = new Serie("The Sopranos", "David Chase", 1999, 5);
+const services = new SeriesService();
+const seriesList = services.getSerie(); 
+console.log(seriesList);
 
 const app = document.querySelector(".container");
 
-new Page (app, [theSopranos]);
+const pendingSeries = seriesList.filter( (serie) => serie.watched===false);
+const watchedSeries = seriesList.filter( (serie) => serie.watched===true);
+
+new Page (app, pendingSeries, watchedSeries);
